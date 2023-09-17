30a545b53030572dd65eec314b5b5738373ac416
Egor Panevin <predsedatel228@yandex.ru>
2023-09-17 15:19:00 +0300

    add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,35 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+      this.array = [];
+      this.start = 0;
+      
+      for (this.i = min; this.i<=max; this.i++) {
+        this.array.push(this.i);
+      }
+      this.start = 0;
+      this.end = this.array.length - 1;
+      this.guess(this.array)
     }
 
     guess() {
-
+      this.mid = Math.round((this.end - this.start)/2) + this.start;
+      return this.array[this.mid]
     }
 
     lower() {
-
+      this.end = this.mid;
+      this.guess()
     }
 
     greater() {
-
+      this.start = this.mid;
+      this.guess()
     }
+
 }
 
 module.exports = GuessingGame;
+
+
+
